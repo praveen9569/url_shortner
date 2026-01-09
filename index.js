dbe60d691e4741f921dd2728e9cd99252a62061f
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2026-01-10 02:16:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 import express from 'express';
 import userRouter from './routes/user.routes.js';
-import { authenticationMiddleware} from './middlewares/auth.middleware.js';
+import { authenticationMiddleware } from './middlewares/auth.middleware.js';
 import urlRouter from './routes/url.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // CORS middleware
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.ORIGIN
+];
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
+
   next();
 });
 
@@ -33,5 +43,5 @@ app.use('/user', userRouter);
 app.use('/', urlRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
