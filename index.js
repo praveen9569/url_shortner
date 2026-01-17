1e0228bcb31a6d6bd7bd5e466a55348c7be51012
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2026-01-17 12:23:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.use(urlRouter);
 /* =========================
    START SERVER
 ========================= */
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Server is running on port ${PORT}`);
 });
